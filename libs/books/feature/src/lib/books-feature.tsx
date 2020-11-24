ddf73128dc0aed065da234a4c7abe6c375ed452b
ransolo <ransolo@me.com>
2020-11-24 11:48:06 -0600

    add-books-and-book-ui-conponents-and-some-stubbed-data

--- libs/books/feature/src/lib/books-feature.tsx
+++ libs/books/feature/src/lib/books-feature.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-/* eslint-disable-next-line */
-export interface BooksFeatureProps {}
-
-const StyledBooksFeature = styled.div`
-  color: pink;
-`;
-
-export function BooksFeature(props: BooksFeatureProps) {
-  return (
-    <StyledBooksFeature>
-      <h1>Welcome to books-feature!</h1>
-    </StyledBooksFeature>
+import { getBooks } from '@neighborly/books/data-access';
+import { Books, Book } from '@neighborly/books/ui';
+export const BooksFeature = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(
+    () => {
+      getBooks().then(setBooks);
+    },
+    [
+      // This effect runs only once on first component render
+      // so we declare it as having no dependent state.
+    ]
   );
-}
-
+  return (
+    <>
+      <h2>Books</h2>
+      <Books books={books} />
+    </>
+  );
+};
 export default BooksFeature;
