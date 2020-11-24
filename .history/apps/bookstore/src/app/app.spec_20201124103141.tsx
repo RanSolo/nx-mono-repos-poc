443bedbbb0274dea5a521030f66e87edea86f318
ransolo <ransolo@me.com>
2020-11-24 10:44:38 -0600

    initial commit

--- .history/apps/bookstore/src/app/app.spec_20201124103141.tsx
+++ .history/apps/bookstore/src/app/app.spec_20201124103141.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import App from './app';
+
 describe('App', () => {
-  afterEach(cleanup);
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
+
     expect(baseElement).toBeTruthy();
   });
-  it('should have a header as the title', () => {
+
+  it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText('Bookstore')).toBeTruthy();
+
+    expect(getByText('Welcome to bookstore!')).toBeTruthy();
   });
 });
